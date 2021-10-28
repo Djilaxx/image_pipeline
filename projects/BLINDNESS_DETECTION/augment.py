eba9c8acc93c66aeac0fc1c1f605a8bb33961c9d
Djilaxx <46345110+Djilaxx@users.noreply.github.com>
2021-10-28 11:54:10 +0200

    switch to cv2 and albumentation
    - switching from pillow to cv2 for image reading
    - switching from torch transforms to albumentations for data aug

--- projects/BLINDNESS_DETECTION/augment.py
+++ projects/BLINDNESS_DETECTION/augment.py
@@ -1,29 +1,30 @@
-import torchvision.transforms as transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 #The mean and std I use are the values from the ImageNet dataset
 #The augmentations are used to make training harder and more robust to novel situations.
 #We don't use augment on the validation set other than normalization to try and estimate the real power of the model in the wild.
 Augmentations = {
     'train':
-        transforms.Compose(
+        A.Compose(
             [
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomVerticalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]
+                A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+                A.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0,),
+                ToTensorV2()
+            ],
         ),
     'valid':
-        transforms.Compose(
+        A.Compose(
             [
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]
+                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0,),
+                ToTensorV2()
+            ],
         ),
     'test':
-        transforms.Compose(
+        A.Compose(
             [
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]
+                A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0,),
+                ToTensorV2()
+            ],
         )
 }
